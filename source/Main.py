85e545c4a3bec8ae959463ca526f939acbc7a06a
Kieran Winfield <kieran.winfield464@gmail.com>
2019-03-01 15:25:18 +0000

    Created new function to handle what the user wants. Gave Menu user input

--- source/Main.py
+++ source/Main.py
@@ -9,9 +9,32 @@ def menu():
     print('2. Select file')
     print('3. Help')
     print('4. Quit')
+    print('Your Selection: ')
+    UserSelection = int(input())
+    return UserSelection
+
+
+def RunSelectedOption(UserOption):
+    if UserOption == 1:
+        Selecion = PickElement.SelectRandomElementFromList()
+        return Selecion
+    elif UserOption == 2:
+        pass
+    elif UserOption == 3:
+        pass
+    elif UserOption == 4:
+        quit()
+    else:
+        print('Did not reconise input! Please make sure you type one number!')
+        return 0
+
 
 def main():
-    Selecion = PickElement.SelectRandomElementFromList()
-    return Selecion
+    while(True):
+        UserOption = menu()
+        ReturnedValue = RunSelectedOption(UserOption)
+        if ReturnedValue != 0:
+            break
+    return print(ReturnedValue)
 
-print(main())
+main()
