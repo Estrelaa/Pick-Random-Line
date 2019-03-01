00a4b0cf913427ef9e9ca004c9e261b93597a68c
Kieran Winfield <kieran.winfield464@gmail.com>
2019-03-01 14:51:34 +0000

    Begun work on a basic menu and a comment at the top.

--- source/Main.py
+++ source/Main.py
@@ -1,4 +1,14 @@
+# This is the main file that runs the whole program. 
+
 import PickElement
+
+def menu():
+    print('Welcome to PRL! Please select a option below: ')
+    print (' ') # Blank line to make menu look nicer
+    print('1. Run program')
+    print('2. Select file')
+    print('3. Help')
+    print('4. Quit')
 
 def main():
     Selecion = PickElement.SelectRandomElementFromList()
